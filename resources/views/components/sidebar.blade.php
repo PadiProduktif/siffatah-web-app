40ab7d11b6b3dba7e88b4cf587bc3c1bdedd1912
Nurrochmat Saptoaji <nurrochmat.sapto@gmail.com>
2024-12-20 21:12:26 +0700

    edit sidebar by role

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -9,54 +9,59 @@
     <p class="text-muted" style="font-size: 12px;">Sistem Informasi Fasilitas dan Kesehatan</p>
     <ul class="nav flex-column">
         <li><a href="/admin/dashboard" class="{{ Request::is('home') ? 'active' : '' }}">Home</a></li>
-        <li><a href="/admin/restitusi_karyawan" class="{{ Request::is('restitusi-karyawan') ? 'active' : '' }}">Restitusi Karyawan</a></li>
-        <!-- Pengajuan Klaim harunya dropdown -->
-        <!-- <li><a href="/admin/klaim_kecelakaan" class="{{ Request::is('pengajuan-klaim') ? 'active' : '' }}">Pengajuan Klaim</a></li> -->
-        <!-- <li><a href="/kepesertaan-anggota" class="{{ Request::is('kepesertaan-anggota') ? 'active' : '' }}">Kepesertaan Anggota</a></li> -->
-        <li><a href="/admin/ekses" class="{{ Request::is('ekses') ? 'active' : '' }}">Ekses</a></li>
-        <li><a href="/admin/berkas_pengobatan" class="{{ Request::is('berkas-pengobatan') ? 'active' : '' }}">Berkas Pengobatan</a></li>
-        <li><a href="/admin/kelengkapan_kerja" class="{{ Request::is('berkas-pengobatan') ? 'active' : '' }}">Kelengkapan Kerja</a></li>
-        <li class="nav-item dropdown">
-            <a href="#" 
-               class="nav-link dropdown-toggle {{ Request::is('wearpack') || Request::is('sepatu') ? 'active' : '' }}" 
-               id="kelengkapanKerjaDropdown" 
-               role="button" 
-               data-bs-toggle="dropdown" 
-               aria-expanded="false">
-                Klaim Asuransi
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="kelengkapanKerjaDropdown">
-                <li><a href="/admin/klaim_pengobatan" class="dropdown-item {{ Request::is('wearpack') ? 'active' : '' }}">Pengobatan</a></li>
-                <li><a href="/admin/klaim_kecelakaan" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Kecelakaan</a></li>
-                <li><a href="/admin/klaim_kematian" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Kematian</a></li>
-                <li><a href="/admin/klaim_purna-jabatan" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Purna jabatan</a></li>
-                <li><a href="/admin/klaim_lumpsum-kacamata" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Lumpsum kacamata</a></li>
-                <li><a href="/admin/klaim_lumpsum-lahiran" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Lumpsum lahiran</a></li>
-            </ul>
-        </li>
-        <!-- <li class="nav-item dropdown">
-            <a href="#" 
-               class="nav-link dropdown-toggle {{ Request::is('wearpack') || Request::is('sepatu') ? 'active' : '' }}" 
-               id="kelengkapanKerjaDropdown" 
-               role="button" 
-               data-bs-toggle="dropdown" 
-               aria-expanded="false">
-                Kelengkapan Kerja
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="kelengkapanKerjaDropdown">
-                <li><a href="/admin/kelengkapan_kerja/wearpack" class="dropdown-item {{ Request::is('wearpack') ? 'active' : '' }}">Wearpack</a></li>
-                <li><a href="/admin/kelengkapan_kerja/sepatu" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Sepatu Safety</a></li>
-                <li><a href="/admin/kelengkapan_kerja/wearpack" class="dropdown-item {{ Request::is('wearpack') ? 'active' : '' }}">Sepatu Kantor</a></li>
-                <li><a href="/admin/kelengkapan_kerja/sepatu" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Jaket Shift</a></li>
-                <li><a href="/admin/kelengkapan_kerja/sepatu" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Seragam Olahraga</a></li>
-                <li><a href="/admin/kelengkapan_kerja/sepatu" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Jaket Casual</a></li>
-                <li><a href="/admin/kelengkapan_kerja/sepatu" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Seragam Dinas Harian</a></li>
-            </ul>
-<<<<<<< HEAD
-        </li> -->       
-        @if(auth()->check() && auth()->user()->role === 'superadmin')
-            <li><a href="/admin/master_data_karyawan" class="{{ Request::is('master-data-karyawan') ? 'active' : '' }}">Master Data Karyawan</a></li>
+        
+        @if(auth()->check() && (auth()->user()->role === 'superadmin' || auth()->user()->role === 'dr_hph'))
+
+            <li><a href="/admin/restitusi_karyawan" class="{{ Request::is('restitusi-karyawan') ? 'active' : '' }}">Restitusi Karyawan</a></li>
+            <li><a href="/admin/ekses" class="{{ Request::is('ekses') ? 'active' : '' }}">Ekses</a></li>
+            <li><a href="/admin/berkas_pengobatan" class="{{ Request::is('berkas-pengobatan') ? 'active' : '' }}">Berkas Pengobatan</a></li>
+            <li class="nav-item dropdown">
+                <a href="#" 
+                class="nav-link dropdown-toggle {{ Request::is('wearpack') || Request::is('sepatu') ? 'active' : '' }}" 
+                id="kelengkapanKerjaDropdown" 
+                role="button" 
+                data-bs-toggle="dropdown" 
+                aria-expanded="false">
+                    Klaim Asuransi
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="kelengkapanKerjaDropdown">
+                    <li><a href="/admin/klaim_pengobatan" class="dropdown-item {{ Request::is('wearpack') ? 'active' : '' }}">Pengobatan</a></li>
+                    <li><a href="/admin/klaim_kecelakaan" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Kecelakaan</a></li>
+                    <li><a href="/admin/klaim_kematian" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Kematian</a></li>
+                    <li><a href="/admin/klaim_purna-jabatan" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Purna jabatan</a></li>
+                    <li><a href="/admin/klaim_lumpsum-kacamata" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Lumpsum kacamata</a></li>
+                    <li><a href="/admin/klaim_lumpsum-lahiran" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Lumpsum lahiran</a></li>
+                </ul>
+            </li>
+            
+            @if(auth()->user()->role === 'superadmin')
+                <li><a href="/admin/kelengkapan_kerja" class="{{ Request::is('berkas-pengobatan') ? 'active' : '' }}">Kelengkapan Kerja</a></li>
+                <li><a href="/admin/master_data_karyawan" class="{{ Request::is('master-data-karyawan') ? 'active' : '' }}">Master Data Karyawan</a></li>
+            @endif
+        @else
+            <li><a href="/ekses" class="{{ Request::is('ekses') ? 'active' : '' }}">Ekses</a></li>
+            <li><a href="/restitusi_karyawan" class="{{ Request::is('restitusi-karyawan') ? 'active' : '' }}">Restitusi Karyawan</a></li>
+            <li><a href="/berkas_pengobatan" class="{{ Request::is('berkas-pengobatan') ? 'active' : '' }}">Berkas Pengobatan</a></li>
+            <li class="nav-item dropdown">
+                <a href="#" 
+                class="nav-link dropdown-toggle {{ Request::is('wearpack') || Request::is('sepatu') ? 'active' : '' }}" 
+                id="kelengkapanKerjaDropdown" 
+                role="button" 
+                data-bs-toggle="dropdown" 
+                aria-expanded="false">
+                    Klaim Asuransi
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="kelengkapanKerjaDropdown">
+                    <li><a href="/klaim_pengobatan" class="dropdown-item {{ Request::is('wearpack') ? 'active' : '' }}">Pengobatan</a></li>
+                    <li><a href="/klaim_kecelakaan" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Kecelakaan</a></li>
+                    <li><a href="/klaim_kematian" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Kematian</a></li>
+                    <li><a href="/klaim_purna-jabatan" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Purna jabatan</a></li>
+                    <li><a href="/klaim_lumpsum-kacamata" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Lumpsum kacamata</a></li>
+                    <li><a href="/klaim_lumpsum-lahiran" class="dropdown-item {{ Request::is('sepatu') ? 'active' : '' }}">Lumpsum lahiran</a></li>
+                </ul>
+            </li>
         @endif
+
         <li><a href="/set-profil" class="{{ Request::is('set-profil') ? 'active' : '' }}">Pengaturan Profil</a></li>
         <li><a href="/logout">Logout</a></li>
     </ul>
