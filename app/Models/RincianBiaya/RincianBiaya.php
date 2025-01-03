d0da203fb25f508d89d3615fdafb8e63de1fa206
PadiProduktif <kantororyza11@gmail.com>
2025-01-03 16:11:49 +0700

    Rincian Biaya Model

---
+++ app/Models/RincianBiaya/RincianBiaya.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\RincianBiaya;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RincianBiaya extends Model
+{
+    use HasFactory;
+    protected $table = 'table_rincian_biaya';
+    protected $primaryKey = 'id_rincian_biaya';
+    protected $fillable = [
+        'id_rincian_biaya',
+        'id_badge',
+        'kategori',
+        'rumah_sakit',
+        'no_surat_rs',
+        'nominal',
+        'created_at',
+        'created_by',
+        'updated_at',
+        'updated_by'
+    ];
+}
